381bf461aabedfa812d6e4f62c244d7e5657009f
bhagya2002 <bhagya02.p@gmail.com>
2021-06-23 23:21:25 -0600

    Initial commit

---
+++ Youtube/1_print.py
@@ -0,0 +1,2 @@
+# Hello world
+print('hello world!') # print to a screen using this "print" function
---
+++ Youtube/2_printshapes.py
@@ -0,0 +1,6 @@
+# drawing shapes
+print('     /|')
+print('    / |')
+print('   /  |')
+print('  /   |')
+print(' /____|')
