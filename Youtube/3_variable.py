d4a13e43a18731533504c24228a75451c398c6bb
bhagya2002 <bhagya02.p@gmail.com>
2021-06-24 10:16:02 -0600

    day 1

---
+++ Youtube/3_variable.py
@@ -0,0 +1,15 @@
+#variables
+character_name = "Tom"
+character_age = "35"
+is_old = True
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old.")
+
+character_name = "Mike" # variable gets updated (TOP TO BOTTOM)
+print("He really liked the name " + character_name + " ,")
+print("but didn't like being " + character_age + " .")
+
+# " " -> string (any text)
+# num -> number (any number including decimals)
+# True/False -> boolean (true or false statements)
