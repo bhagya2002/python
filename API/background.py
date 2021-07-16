d6d980058fb10b17a1147b8eb106291b72000909
bhagya2002 <bhagya02.p@gmail.com>
2021-07-16 00:19:06 -0600

    Python API Learning Init

---
+++ API/background.py
@@ -0,0 +1,13 @@
+# API (Application Programming Interface) - API acts as a layer between your application and external servic
+
+# REST API (Representational state transfer)
+
+# 1. Client-server architecture – the client is responsible for the user interface, and the server is responsible for the backend and data storage. Client and server are independent and each of them can be replaced.
+# 2. Stateless – no data from the client is stored on the server side. The session state is stored on the client side.
+# 3. Cacheable – clients can cache server responses to improve performance.
+
+# Types of HTTPS requests
+# 1. GET – retrieve information (like search results). Using it, we can get the data we are interested in from those that the API is ready to share.
+# 2. POST – adds new data to the server. Using this type of request, you can, for example, add a new item to your inventory
+# 3. PUT – changes existing information. For example, using this type of request, it would be possible to change the color or value of an existing product
+# 4. DELETE – deletes existing information
